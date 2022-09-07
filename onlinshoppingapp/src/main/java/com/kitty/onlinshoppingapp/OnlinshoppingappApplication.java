51be06bd8627b395af1d3dd8d0991606842f9d39
pangyongxin24 <112561977+pangyongxin24@users.noreply.github.com>
2022-09-07 10:22:28 +0800

    updates

---
+++ onlinshoppingapp/src/main/java/com/kitty/onlinshoppingapp/OnlinshoppingappApplication.java
@@ -0,0 +1,13 @@
+package com.kitty.onlinshoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinshoppingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinshoppingappApplication.class, args);
+    }
+
+}
