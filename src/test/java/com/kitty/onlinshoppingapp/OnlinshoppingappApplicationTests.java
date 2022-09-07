c352986051f52c4169dfdd10e53df6805601cc3b
SerenaPang <ypang4@usfca.edu>
2022-09-07 10:16:36 +0800

    first commmit

---
+++ src/test/java/com/kitty/onlinshoppingapp/OnlinshoppingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitty.onlinshoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinshoppingappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
